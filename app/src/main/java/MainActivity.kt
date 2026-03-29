bc44a2d05cc1bc5f041a9b1bae493c357aa1c3da
tt3cpl <sershkolbol@gmail.com>
2026-03-29 21:55:12 +0300

    fix #1

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -9,9 +9,27 @@ import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        val math_operation = findViewById<TextView>(R.id.math_operation)
+        val result_text = findViewById<TextView>(R.id.result_text)
+
+        outState.putString("math_operation", math_operation.text.toString())
+        outState.putString("result_text", result_text.text.toString())
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState != null) {
+            val math_operation = findViewById<TextView>(R.id.math_operation)
+            val result_text = findViewById<TextView>(R.id.result_text)
+
+            math_operation.text = savedInstanceState.getString("math_operation", "")
+            result_text.text = savedInstanceState.getString("result_text", "")
+        }
 
         val but_0 = findViewById<TextView>(R.id.but_0)
         but_0.setOnClickListener { setTextFields("0") }
